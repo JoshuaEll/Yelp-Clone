59148a8abc609809e32f75d629be5101a01fe34c
Joshua Ellis <joshuaell131@gmail.com>
2023-04-11 22:55:05 -0500

    restart of backend part of project

--- backend/app.js
+++
@@ -1,9 +0,0 @@
-// Needs:
-// -user registration and login backend (passport or jwebtokens or oAUTH2)
-// -allow upload of files(png, jpeg)
-// -creation of Businesses
-// -
-
-
-// Maybe:
-// - add ability to change users if business owner can verify (add verification form)
