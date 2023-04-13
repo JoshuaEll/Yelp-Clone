41de6e23cd18c3362869138a867e5620b9120307
Joshua Ellis <joshuaell131@gmail.com>
2023-04-12 19:58:29 -0500

    finished sql file

--- backend/data.sql
+++ backend/data.sql
@@ -1,18 +1,27 @@
 CREATE DATABASE yelp;
 
 CREATE TABLE user (
-    email VARCHAR(255) PRIMARY KEY,
+    user_email VARCHAR(255) PRIMARY KEY,
     hashed_password VARCHAR(255),
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE comments (
+    comment_id SERIAL PRIMARY KEY,
+    user_email VARCHAR(255),
+    business_id VARCHAR(255),
+    comment VARCHAR(255),
+    CONSTRAINT fk_businessID FOREIGN KEY(business_id) REFERENCES business(business_id),
+    CONSTRAINT fk_userEmail FOREIGN KEY(user_email) REFERENCES user(user_email)
+)
 CREATE TABLE pictures (
-    id VARCHAR(255) PRIMARY KEY,
+    business_id VARCHAR(255) PRIMARY KEY,
     picture_name VARCHAR(255),
-    user_id VARCHAR(255),
-    CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES user(id)
+    user_email VARCHAR(255),
+    picture_location VARCHAR(100),
+    CONSTRAINT fk_user FOREIGN KEY(user_email) REFERENCES user(user_email)
 );
 
 CREATE TABLE business (
@@ -21,8 +30,7 @@ CREATE TABLE business (
     business_address VARCHAR(300),
     business_pictures TEXT[],
     user_email VARCHAR(255),
-    comments TEXT,
     rating DECIMAL,
     CONSTRAINT fk_pictures FOREIGN KEY(business_pictures) REFERENCES pictures(id),
-    CONSTRAINT fk_userId FOREIGN KEY(user_email) REFERENCES user(email)
+    CONSTRAINT fk_userId FOREIGN KEY(user_email) REFERENCES user(user_email)
 );
